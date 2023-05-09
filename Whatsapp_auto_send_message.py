27967fc78d4cb7a6e04a8880cdf18e715ad5b8cf
Enes Ünver <enes12_34@hotmail.com>
2023-05-09 18:39:17 +0300

    Add send message to multiple people

--- Whatsapp_auto_send_message.py
+++ Whatsapp_auto_send_message.py
@@ -16,21 +16,21 @@ input("Please enter the any key for continui after scan QR")
 
 message = "Hello (this is Auto message)"
 
-contact_name = "Hikmet"
-
-# Wait find chatbox
-search_box = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, f"//span[@title='{contact_name}']")))
-# Auto click on the finded person
-search_box.click()
-# Wait find the wanted person message box
-chat_box = WebDriverWait(driver, 20).until(
-    EC.presence_of_element_located((By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p')))
-# Click the message box
-ActionChains(driver).move_to_element(chat_box).click().perform()
-# Auto write message and send
-chat_box.send_keys(message)
-chat_box.send_keys(Keys.RETURN)
-time.sleep(3)
+contact_name = ["Bedo","Kubi"]
+for i in contact_name:
+    # Wait find chatbox
+    search_box = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, f"//span[@title='{i}']")))
+    # Auto click on the finded person
+    search_box.click()
+    # Wait find the wanted person message box
+    chat_box = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p')))
+    # Click the message box
+    ActionChains(driver).move_to_element(chat_box).click().perform()
+    # Auto write message and send
+    chat_box.send_keys(message)
+    chat_box.send_keys(Keys.RETURN)
+    time.sleep(3)
 # Quit the browser
 driver.quit()
